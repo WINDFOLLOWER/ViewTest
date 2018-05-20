ed4246092635d30ff4d78614eab899b7b2c22f29
Wind7 <sunchen5575@126.com>
2018-05-21 00:08:20 +0800

    0521第一次提交

--- app/src/main/java/com/example/dell/viewtest/MainActivity.java
+++ app/src/main/java/com/example/dell/viewtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dell.viewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         PieData pieData1 = new PieData("A", 12.2f);
         PieData pieData2 = new PieData("B", 6.2f);
         mData.add(pieData1);
@@ -20,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
         PieView pieView = findViewById(R.id.pie_view);
         pieView.setMStartAngle(120);
         pieView.setMPieData(mData);
+        */
+
+        final CheckView checkView = findViewById(R.id.check_view);
+        checkView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        Button buttonCheck = findViewById(R.id.check);
+        Button buttonUncheck = findViewById(R.id.uncheck);
+
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkView.check();
+            }
+        });
+
+        buttonUncheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkView.unCheck();
+            }
+        });
+
     }
 }
